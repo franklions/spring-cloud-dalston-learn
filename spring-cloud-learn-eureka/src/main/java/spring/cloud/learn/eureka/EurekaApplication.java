eb9ec442672404c8026a75bef142ea362831b1dd
flsh <fenglianshuang@jiuan.com>
2018-03-29 17:14:41 +0800

    创建Eureka项目

---
+++ spring-cloud-learn-eureka/src/main/java/spring/cloud/learn/eureka/EurekaApplication.java
@@ -0,0 +1,21 @@
+package spring.cloud.learn.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author flsh
+ * @version 1.0
+ * @description
+ * @date 2018/3/29
+ * @since Jdk 1.8
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class,args);
+    }
+}
